fe8ca866908b9706757ee0a8de88886d12eda057
FabiVGroellF <fgroellfrontado@frba.utn.edu.ar>
2025-06-19 01:43:11 +0000

    Documentación de utils de Server

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -15,21 +15,50 @@ type Paquete struct {
 }
 
 func RecibirPaquetes(w http.ResponseWriter, r *http.Request) {
+	// RecibirPaquetes recibe 2 argumentos:
+	// w es un http.ResponseWriter que se usa para enviar
+	// la respuesta al cliente.
+	// r es un *http.Request que contiene la información de la
+	// petición HTTP recibida.
+
 	decoder := json.NewDecoder(r.Body)
+	// Se crea un nuevo decodificador de JSON
+	// que usará el body del request recibido.
+
 	var paquete Paquete
+	// Se declara una variable paquete de tipo struct Paquete
+	// que contendrá los datos decodificados del paquete recibido.
+
 	err := decoder.Decode(&paquete)
+	// El decoder decodifica el body del request
+	// y lo guarda en la variable paquete.
+	// Si hay un error al decodificar, se guarda en err.
+
 	if err != nil {
 		log.Printf("error al decodificar mensaje: %s\n", err.Error())
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("error al decodificar mensaje"))
 		return
 	}
+	// Si hay un error al decodificar el paquete,
+	// se loggea un mensaje personalizado con la descripción del error,
+	// WhiteHeader cambia el estado del ResponseWriter a 400 (Bad Request),
+	// Write escribe una respuesta al cliente en el ResponseWriter.
+	// Luego se sale de la función con return.
 
-	log.Println("me llego un paquete de un cliente")
+	log.Println("Me llegó un paquete de un cliente")
+	// Si no hay error, se loggea un mensaje personalizado
+	// con salto de linea al final.
+
 	log.Printf("%+v\n", paquete)
+	// Se loggea el paquete recibido.
+	// %+v imprime todos los campos del struct paquete con sus valores.
 
 	w.WriteHeader(http.StatusOK)
+	// Se cambia el estado del ResponseWriter a 200 (OK).
+
 	w.Write([]byte("ok"))
+	// Se escribe la respuesta al cliente en el ResponseWriter.
 }
 
 func RecibirMensaje(w http.ResponseWriter, r *http.Request) {
